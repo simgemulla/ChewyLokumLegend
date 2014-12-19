5aa64cf5d1f533f309d99eed8151471556290534
simgemulla <smullaoglu@ku.edu.tr>
2014-12-19 15:48:13 +0200

    TimedLokum ve TimedLevel - taslak
    TimedLokum ve TimedLevel - taslak

---
+++ ChewyLokumLegend/src/DreamPackage/TimedLevel.java
@@ -0,0 +1,11 @@
+package DreamPackage;
+
+public class TimedLevel extends Level {
+
+	public TimedLevel(int moveCount, int highScore, Lokum[][] initialBoard,
+			int scoreNeeded, int levelID, boolean locked) {
+		super(moveCount, highScore, initialBoard, scoreNeeded, levelID, locked);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ChewyLokumLegend/src/DreamPackage/TimedLokum.java
@@ -0,0 +1,15 @@
+package DreamPackage;
+
+import java.awt.Color;
+
+public class TimedLokum extends Lokum {
+
+	public TimedLokum(Color c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+}
